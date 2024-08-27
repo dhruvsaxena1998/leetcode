1b97187e06b11a5ff6ff8361339295b9004c368d
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2024-08-27 08:24:40 +0530

    day 5

---
+++ 592 . Fraction Addition and Subtraction.js
@@ -0,0 +1,18 @@
+// Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+// The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+// Input: expression = "-1/2+1/2"
+// Output: "0/1"
+
+// Input: expression = "-1/2+1/2+1/3"
+// Output: "1/3"
+
+// Input: expression = "1/3-1/2"
+// Output: "-1/6"
+
+const fractionAddition = (expression) => {};
+
+// console.log(fractionAddition("-1/2+1/2"), "0/1");
+console.log(fractionAddition("-1/2+1/2+1/3"), "1/3");
+// console.log(fractionAddition("1/3-1/2"), "1/6");
